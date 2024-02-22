161af8111f26f089bbb6ecb2aeb3c2b69788686d
JAYBORICHA <jayboricha707@gmail.com>
2024-02-22 12:16:11 +0530

    Created basic bolierplate for frontend and backend

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+import cors from  "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+export { app }
