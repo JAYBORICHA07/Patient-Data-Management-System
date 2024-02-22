a40bbc01d82c0c5f63abc3da1f67073ddce7a046
JAYBORICHA <jayboricha707@gmail.com>
2024-02-22 18:44:40 +0530

    Configured routing using react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import Homepage from "./pages/Homepage.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Homepage />,
+    errorElement : <ErrorPage />
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <RouterProvider router={router}/>
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,7 @@
+const ErrorPage = () => {
+  return (
+    <div>404 - Page not found</div>
+  )
+}
+
+export default ErrorPage
