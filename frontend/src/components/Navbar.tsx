bd89df2a307179313f5ba14f5c7578830c744ee2
JAYBORICHA <jayboricha707@gmail.com>
2024-02-24 23:04:32 +0530

    Compeleted Navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,108 @@
+import { Button } from "./ui/button";
+import { LogInIcon, UserRoundPlus } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "./ui/separator";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="w-full p-4 m-0 h-16 sm:flex justify-between items-center hidden">
+        <div className="flex justify-start items-center gap-3">
+          <img
+            src="../../public/vite.svg"
+            alt="CompanyLogo"
+            className="h-16 md:h-14"
+          />
+          <p className="hidden md:block">Patient Management System</p>
+        </div>
+        <div className="flex justify-end items-center gap-2">
+          <Button variant={"ghost"}>
+            <a href="/home">Home</a>
+          </Button>
+          <Button variant={"ghost"}>
+            <a href="/about">About Us</a>
+          </Button>
+          <Button variant={"ghost"}>
+            <a href="/contactus">Contact Us</a>
+          </Button>
+          <Button variant={"outline"}>
+            <a
+              href="register"
+              className="flex justify-center items-center gap-2"
+            >
+              <UserRoundPlus size={"18px"} />
+              Register
+            </a>
+          </Button>
+          <Button>
+            <a href="/login" className="flex justify-center items-center gap-2">
+              {" "}
+              <LogInIcon size={"18px"} />
+              Login
+            </a>
+          </Button>
+        </div>
+      </div>
+      <div className="flex p-4 m-0 justify-end items-center sm:hidden">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>Menu</Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+            <div className="flex flex-col gap-2">
+              <Button variant={"ghost"}>
+                <a href="/home">Home</a>
+              </Button>
+              <Separator />
+              <Button variant={"ghost"}>
+                <a href="/about">About Us</a>
+              </Button>
+              <Separator />
+              <Button variant={"ghost"}>
+                <a href="/contactus">Contact Us</a>
+              </Button>
+              <Separator />
+              <Button variant={"outline"}>
+                <a
+                  href="register"
+                  className="flex justify-center items-center gap-2"
+                >
+                  <UserRoundPlus size={"18px"} />
+                  Register
+                </a>
+              </Button>
+              {/* <Separator /> */}
+              <Button>
+                <a
+                  href="/login"
+                  className="flex justify-center items-center gap-2"
+                >
+                  {" "}
+                  <LogInIcon size={"18px"} />
+                  Login
+                </a>
+              </Button>
+              <Separator />
+            </div>
+            <SheetFooter>
+              {/* <SheetClose asChild>
+                <Button type="submit">Save changes</Button>
+              </SheetClose> */}
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
+      </div>
+    </>
+  );
+};
+export default Navbar;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,21 +5,23 @@ import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import Homepage from "./pages/Homepage.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
-
+import Navbar from "./components/Navbar.tsx";
 
 const router = createBrowserRouter([
   {
-    path : '/',
-    element : <Homepage />,
-    errorElement : <ErrorPage />
-  }
-])
-
+    path: "/",
+    element: <Homepage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-      <RouterProvider router={router}/>
+      <div className="mx-5">
+        <Navbar />
+        <RouterProvider router={router} />
+      </div>
     </ThemeProvider>
   </React.StrictMode>
 );
