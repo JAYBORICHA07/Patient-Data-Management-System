a5564683d6b87802e9f2303185612b309d926c6b
JAYBORICHA <jayboricha707@gmail.com>
2024-02-26 16:47:08 +0530

    Started with the login/signup route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv'
 import { app } from './app.js';
+import { supabase } from './db/supabase.js';
 
 dotenv.config({
     path : './env'
 })
 
-app.listen(process.env.PORT || 8000, () => {
+app.listen(process.env.PORT || 8000, async () => {
     console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    const {data, error} = await supabase.from('users').select()
+    if(error){
+        console.log(error)
+    }else{
+        console.log(data)
+    }
 })
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,13 +1,36 @@
 import { Router } from "express";
+import { supabase } from "../db/supabase.js"
 
 const router = Router();
 
-router.route("/register").post((req, res) => {
-    res.json({
-        messege : "working"
-    })
+router.route("/register").post(async (req, res) => {
+    const {name, email, phoneNumber, role, password} = req.body
+    
+    const {data, error} = await supabase.from('users').insert({
+        name,
+        email,
+        phoneNumber,
+        role,
+        password
+    }).select()
+    if(error){
+        res.send(error)
+    }else{
+        res.send(data)
+    }
 })
 
+router.route("/login").post(async (req, res) => {
+    const {email, password} = req.body
+
+    const {data, error} = await supabase.from('users').select('*').eq('email',email).eq('password',password)
+    if(error){
+        res.send(error)
+    }else{
+        res.send(data)
+    }
+})
+ 
 
 
 export default router
