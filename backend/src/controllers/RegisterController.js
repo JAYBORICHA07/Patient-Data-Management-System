4d351fbe7402db73b2c56bb42c82527ec2d85645
KeraliyaDisha <dishakeraliya08@gmail.com>
2024-02-29 21:51:44 +0530

    password encryption and access token set

--- backend/src/controllers/RegisterController.js
+++ backend/src/controllers/RegisterController.js
@@ -1,15 +1,17 @@
 import { supabase } from "../db/supabase.js"
+import encrypt from "../lib/helper.js"
 
 const RegisterController = async (req, res) => {
     const { name, email, phoneNumber, role, password } = req.body
     console.log({name, email, phoneNumber, role, password})
-
+    
+    const hashedPassword = await encrypt.generatedPassword(password);
     const { data, error } = await supabase.from('users').insert({
         name,
         email,
         phoneNumber,
         role,
-        password
+        password: hashedPassword
     }).select()
     if (error) {
         res.send(error)
